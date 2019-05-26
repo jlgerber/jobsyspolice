8052585a4aad16b8bc07e82a319af2028d46fa97
jonathan gerber <jlgerber@gmail.com>
2019-05-26 10:21:17 -0700

    adds structopt options

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
 use jstemplate2::*;
 use petgraph;
-use petgraph::visit::Bfs;
-use petgraph::visit::IntoNodeReferences;
+//use petgraph::visit::Bfs;
+//use petgraph::visit::IntoNodeReferences;
 use fern;
 use fern::colors::{Color, ColoredLevelConfig};
 use chrono;
 use log;
+use log::LevelFilter;
+use structopt::StructOpt;
+
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "jstest", about = "test jobsystem paths")]
+struct Opt {
+    /// Set logging level to one of trace, debug, info, warn, error
+    #[structopt(short = "l", long = "level", default_value="info")]
+    level: String,
+
+    /// Save out a dot file of the graph
+    #[structopt( long = "dot")]
+    dot: bool,
+
+    /// Input file
+    #[structopt(name="INPUT")]
+    input: Option<String>,
+}
 
 fn setup_logger(level: log::LevelFilter) -> Result<(), fern::InitError> {
     let  colors = ColoredLevelConfig::new()
@@ -31,11 +50,26 @@ fn setup_logger(level: log::LevelFilter) -> Result<(), fern::InitError> {
     Ok(())
 }
 
-fn main() {
-    setup_logger(log::LevelFilter::Trace).unwrap();
+fn setup_cli() -> (Opt, log::LevelFilter) {
+    let args = Opt::from_args();
+    let level = match args.level.as_str() {
+        "trace" => LevelFilter::Trace,
+        "debug" => LevelFilter::Debug,
+        "info"  => LevelFilter::Info,
+        "warn"  | "warning" => LevelFilter::Warn,
+        "err" | "error" => LevelFilter::Error,
+        _ => LevelFilter::Warn,
+    };
 
+    (args, level)
+}
+
+fn main() {
+    let (args, level) = setup_cli();
+    setup_logger(level).unwrap();
     let graph = graph::testdata::build_graph();
     //println!("{:#?}",  petgraph::dot::Dot::with_config(&graph, &[petgraph::dot::Config::EdgeNoLabel]));
+    /*
     for node in graph.node_references() {
         println!("{:?}", node);
     }
@@ -53,7 +87,13 @@ fn main() {
     for n in neighbors {
         println!("{:?}", n);
     }
-
-    let p = "/dd/shows/DEV01/SHARED/MODEL/foo/bar";
-    println!("is {} valid? {}", p, is_valid(p, &graph));
+    */
+    //let p = "/dd/shows/DEV01/SHARED/MODEL/foo/bar";
+    if args.dot {
+        println!("{:#?}",  petgraph::dot::Dot::with_config(&graph, &[petgraph::dot::Config::EdgeNoLabel]));
+    } else if let Some(input) = args.input {
+        println!("{}", is_valid(input.as_str(), &graph));
+    } else {
+        eprintln!("\nPass input to command. See help for more details\n")
+    }
 }
