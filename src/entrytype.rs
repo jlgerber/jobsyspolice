fcac778ec71e8f830fdb1df3276f4718dd7dc21a
jonathan gerber <jlgerber@gmail.com>
2019-05-27 09:45:44 -0700

    cleans up use statements in entrytype.rs

--- src/entrytype.rs
+++ src/entrytype.rs
@@ -1,5 +1,5 @@
-use std::fmt::{Display, Formatter, self};
-use serde::{Deserialize,Serialize};
+use std::fmt::{ Display, Formatter, self };
+use serde::{ Deserialize, Serialize };
 
 #[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Clone, Copy, Deserialize, Serialize)]
 pub enum EntryType {
