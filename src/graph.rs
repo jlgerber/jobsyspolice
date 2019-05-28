c6b8045d5d3a3893aef461a810a6371bc85d7723
jonathan gerber <jlgerber@gmail.com>
2019-05-28 13:54:58 -0700

    adds more dirs graph.rs

--- src/graph.rs
+++ src/graph.rs
@@ -97,12 +97,12 @@ pub mod testdata {
         let shows = graph.add_node(Node::from_str("shows").unwrap());
         let show = graph.add_node(Node::new_regexp("show", r"^[A-Z]+[A-Z 0-9]*$", None));
         let mut refdir = Node::from_str("REF").unwrap();
+
         // ref
         refdir.set_volume();
         let refdir = graph.add_node(refdir);
         let quicktimes = graph.add_node(Node::from_str("quicktimes").unwrap());
         let qtsubdir = graph.add_node(Node::new_regexp("qtsubdir", r"^[0-9_]+$", None));
-
         let mut clientvault = Node::from_str("CLIENT_VAULT").unwrap();
         clientvault.set_volume();
         let clientvault = graph.add_node(clientvault);
@@ -123,51 +123,88 @@ pub mod testdata {
         let chars_sd = graph.add_node(Node::new_regexp("chars_sd",r"^[a-z0-9_]+$", None));
         // the full chars regexp has a lookahead negative which the regexp lib does not support.
         // TODO: add an optional Negative expression to regexp that will reject matches
-        // RegexpWneg
 
-        let tools = graph.add_node(Node::from_str("tools").unwrap());
+        // SHOW
+        let mut client_dd_edit = Node::new_regexp("client_dd_edit", r"^(CLIENT|DD)$", None);
+        client_dd_edit.set_volume();
+        let client_dd_edit = graph.add_node(client_dd_edit);
+        let client_dd_edit_sd = graph.add_node(
+            Node::new_regexp("client_dd_edit_sd",r"^(([0-9]{4,5})|([0-9]{1,2}?[a-z]+)|([a-z]{2}[0-9]{4,5}))$",None)
+        );
+        let tools = graph.add_node(Node::from_str("tools").unwrap()); // 0751 ddinst
+        let logs = graph.add_node(Node::from_str("logs").unwrap()); // 0771
         let package = graph.add_node(Node::from_str("package").unwrap());
         let extension = graph.add_node(Node::from_str("extension").unwrap());
-        let color = graph.add_node(Node::from_str("COLOR").unwrap());
+        let color = graph.add_node(Node::from_str("color").unwrap());
         let category = graph.add_node(Node::new_regexp("category", r"^(char|prop|veh|scene|enviro|kit)$", None));
         let dept = graph.add_node(Node::new_regexp("department", r"^(integ|model|previz|postviz|enviro|rig|anim|fx|cfx|light|comp|lookdev|shotmodel)$", None));
         let subcontext = graph.add_node(Node::new_regexp("subcontext", r"^[a-z]+([_]{0,1}[a-z0-9])*$", None));
         let bin = graph.add_node(Node::from_str("bin").unwrap());
-        let etc = graph.add_node(Node::from_str("etc").unwrap());
-        let user = graph.add_node(Node::from_str("user").unwrap());
+        let etc = graph.add_node(Node::from_str("etc").unwrap()); //0751 ddinst
+        let lib = graph.add_node(Node::from_str("lib").unwrap()); //ddinst
+        let lib_sd = graph.add_node(
+            Node::new_regexp("lib_sd", r"^(config|cortex|dmx|houdini|integ|jstools|katana|lw|massive|max|maya|mentalray|mkfoldy|moco|mova|nfb|nuke|perl|python[0-9.]*|race|refchef|rman|scratch|setupenv|shader|shoot2x|submission|vray|wam|web)$", None) // 0771
+        );
+        let prod = graph.add_node(Node::from_str("prod").unwrap()); // 755
+        let docs = graph.add_node(Node::from_str("docs").unwrap()); // 0771
+        let mut user = Node::from_str("user").unwrap(); // 751
+        user.set_volume();
+        let user = graph.add_node(user);
+        let work = graph.add_node(Node::new_regexp("work", r"^work\.[a-z]+$", None)); // 0770 default 0555
+        let mut outsource = Node::from_str("OUTSOURCE").unwrap();
+        outsource.set_volume();
+        let outsource = graph.add_node(outsource);
+        let outsource_sd = graph.add_node(Node::new_regexp("outsource_sd", r"^[a-zA-Z0-9_.]+$", None)); //perms default 555
+        let outsource_sdd = graph.add_node(Node::new_regexp("outsource_sdd", r"[a-zA-Z0-9_.]+^$", None)); // 0770 (?!(\bprod\b))
+        let finals = graph.add_node(Node::from_str("FINALS").unwrap()); // 750
+        let finals_sd = graph.add_node(Node::new_regexp("finals_sd", r"[0-9_]+", None));
+        let conform = graph.add_node(Node::from_str("CONFORM").unwrap());
+        let conform_sd =graph.add_node(Node::new_regexp("conform_sd", r"^[a-z0-9_]+$", None));
+        // conform can also have SHARED as subdir as well as user docs and prod
+
+        let artdept = graph.add_node(Node::from_str("ARTDEPT").unwrap());
+        let artdept_sd = graph.add_node(Node::new_regexp("artdept_sd", r"^[a-zA-Z0-9_.-]+$", None)); //0770
+        let storyboard = graph.add_node(Node::from_str("STORYBOARD").unwrap());
+        let storyboard_sd = graph.add_node(
+            Node::new_regexp("storyboard_sd", r"^[0-9]{2}_[0-9]{4}$", None)
+        );// 0770
+        let editorial = graph.add_node(Node::from_str("STORYBOARD").unwrap());
+        let film_lens = graph.add_node(Node::new_regexp("film_lens", r"^(FILM|LENS)$", None));
+        let dailies = graph.add_node(Node::from_str("DAILIES").unwrap());
+
         let shared = graph.add_node(Node::from_str("SHARED").unwrap());
         let shared_dirs = graph.add_node(Node::new_regexp("shared_dirs", r"^(PREVIZ|INTEG|MODEL|RIG|ANIM|CFX|LIGHT|ENVIRO|FX|COMP|IMG)$", None));
-        let work = graph.add_node(Node::new_regexp("work", r"^work\.[a-z]+$", None));
-        let sequence = graph.add_node(Node::new_regexp("sequence", r"^(([A-Z]{2,4})|LIBRARY)$", None));
-        let adsequence = graph.add_node(Node::from_str("ASSETDEV").unwrap());
-        let shot = graph.add_node(Node::new_regexp("shot", r"^[0-9]+[A-Z0-9]*$", None));
+        let assetdev = graph.add_node(Node::from_str("ASSETDEV").unwrap());
         let adshot = graph.add_node(Node::new_regexp("assetdev shot", r"^([A-Z][A-Z0-9]+[_]{0,1})+[A-Z0-9]+$", None));
+        let sequence = graph.add_node(Node::new_regexp("sequence", r"^(([A-Z]{2,4})|LIBRARY)$", None));
+        let shot = graph.add_node(Node::new_regexp("shot", r"^[0-9]+[A-Z0-9]*$", None));
 
         graph.extend_with_edges(&[
             (root, dd),
             (dd, shows),
             (shows, show),
             (show, tools),
+            (show, logs),
+            (show, etc),
             (show, refdir),
-            (refdir, quicktimes),
-            (quicktimes, qtsubdir),
-            (refdir, clientvault),
-            (clientvault, clientvaultsd),
-            (clientvaultsd, clientvaultssd),
-            (refdir, slates_n_categories),
-            (slates_n_categories, snc_sd),
-            (refdir, locations),
-            (locations, loc_sd),
-            (loc_sd, loc_ssd),
-            (refdir, documents),
-            (documents, doc_sd),
-            (refdir, audio),
-            (refdir, audio_sd),
-            (refdir, threed),
-            (threed, threed_sd),
-            (refdir, chars),
-            (chars, chars_sd),
-
+              (refdir, quicktimes),
+                (quicktimes, qtsubdir),
+              (refdir, clientvault),
+                (clientvault, clientvaultsd),
+                  (clientvaultsd, clientvaultssd),
+              (refdir, slates_n_categories),
+                (slates_n_categories, snc_sd),
+              (refdir, locations),
+                (locations, loc_sd),
+                  (loc_sd, loc_ssd),
+              (refdir, documents),
+                (documents, doc_sd),
+              (refdir, audio),
+              (refdir, audio_sd),
+              (refdir, threed),
+                (threed, threed_sd),
+              (refdir, chars),
+                (chars, chars_sd),
         ]);
 
         graph.extend_with_edges(&[
@@ -177,10 +214,37 @@ pub mod testdata {
             (show, etc),
             (show, color),
             (show, user),
-            (user, work),
+              (user, work),
+            (show, client_dd_edit),
+              (client_dd_edit, client_dd_edit_sd),
             (show, shared),
-            (shared, etc),
-            (shared, shared_dirs),
+              (shared, etc),
+              (shared, shared_dirs),
+            (show, lib),
+              (lib, lib_sd),
+            (show, prod),
+            (show, docs),
+            (show, outsource),
+              (outsource, outsource_sd),
+                (outsource_sd, outsource_sdd),
+            (show, finals),
+              (finals, finals_sd),
+            (show, conform),
+              (conform, user),
+              (conform, shared),
+              (conform, conform_sd),
+              (conform, docs),
+              (conform, prod),
+        ]);
+
+        graph.extend_with_edges(&[
+            (show, artdept),
+              (artdept, artdept_sd),
+            (show, storyboard),
+              (storyboard, storyboard_sd),
+            (show, editorial),
+            (show, film_lens),
+            (show, dailies),
         ]);
         // split it up because there appears to be
         // a max size for &[]
@@ -189,17 +253,14 @@ pub mod testdata {
             (category, dept),
             (dept, subcontext),
             (show, sequence),
-            (show, adsequence),
+            (show, assetdev),
+            (assetdev, adshot),
             (sequence, tools),
             (sequence, etc),
             (sequence, shared),
             (sequence, user),
             (sequence, shot),
-            (adsequence, tools),
-            (adsequence, etc),
-            (adsequence, shared),
-            (adsequence, user),
-            (adsequence, adshot),
+
         ]);
 
         graph.extend_with_edges(&[
@@ -211,6 +272,8 @@ pub mod testdata {
             (adshot, etc),
             (adshot, shared),
             (adshot, user),
+            (adshot, prod),
+            (adshot, docs),
         ]);
 
         graph
