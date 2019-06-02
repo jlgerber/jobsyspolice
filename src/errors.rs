5088cfba10e22b724d6afc3254bbee6325a8cc9f
jonathan gerber <jlgerber@gmail.com>
2019-06-02 11:45:37 -0700

    adds IoError

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,7 @@
 use failure::Fail;
 use crate::NIndex;
 use std::ffi::OsString;
+use std::io;
 
 #[derive(Debug, Fail)]
 pub enum JSPError {
@@ -10,4 +11,13 @@ pub enum JSPError {
     ValidationFailure{ entry: OsString, node: NIndex, depth: u8 },
     #[fail(display = "Placeholder error")]
     Placeholder,
+    #[fail(display = "{}", _0)]
+    IoError(#[cause] io::Error),
+}
+
+
+impl From<io::Error> for JSPError {
+  fn from(error: io::Error) -> Self {
+        JSPError::IoError(error)
+    }
 }
