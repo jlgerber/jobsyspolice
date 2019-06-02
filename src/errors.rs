9bd6016b638d800246dc80b1e4730e132fbaa363
jonathan gerber <jlgerber@gmail.com>
2019-06-02 15:54:19 -0700

    adds ParseIntError

--- src/errors.rs
+++ src/errors.rs
@@ -2,6 +2,7 @@ use failure::Fail;
 use crate::NIndex;
 use std::ffi::OsString;
 use std::io;
+use std::num;
 
 #[derive(Debug, Fail)]
 pub enum JSPError {
@@ -13,11 +14,19 @@ pub enum JSPError {
     Placeholder,
     #[fail(display = "{}", _0)]
     IoError(#[cause] io::Error),
+    #[fail(display = "{}", _0)]
+    ParseIntError(#[cause] num::ParseIntError),
 }
 
 
 impl From<io::Error> for JSPError {
   fn from(error: io::Error) -> Self {
         JSPError::IoError(error)
+    }
+}
+
+impl From<num::ParseIntError> for JSPError {
+  fn from(error: num::ParseIntError) -> Self {
+        JSPError::ParseIntError(error)
     }
 }
