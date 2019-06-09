6d42b13685c909eb8151fb8dcc2e50d463a8df79
jonathan gerber <jlgerber@gmail.com>
2019-06-09 14:06:58 -0700

    moves FindValue to end of file in find & improves logging

--- src/find.rs
+++ src/find.rs
@@ -3,22 +3,29 @@ use std::{ cell::RefCell, rc::Rc, collections::VecDeque };
 use log;
 use petgraph::{visit::IntoNodeReferences};
 
-#[derive(Debug, PartialEq, Eq)]
-pub enum FindValue {
-    Success(Rc<RefCell<Vec<NIndex>>>),
-    Failure(Rc<RefCell<Vec<NIndex>>>)
-}
 
-impl FindValue {
-    /// Return whether or not the instance of FindValue is Success
-    pub fn is_success(&self) -> bool {
-        match self {
-            FindValue::Success(_) => true,
-            _ => false,
-        }
-    }
-}
-/// Find a NodePath given a vector of criteria
+/// Find a NodePath given a vector of criteria Strings and a JGraph reference
+///
+/// # Parameters
+/// * `criteria` - VecDeque of Strings representing a path through the graph of
+///                RegEx Nodes. The nodes may be non-contiguous as long as they
+///                are in order, and are padded by Simple Nodes.
+/// * `graph` - JGraph reference which is expected to be populated by nodes and
+///             NIndexes prior to calling find.
+/// # Returns
+///    A NodePath if successful. Otherwise, a JSPError
+///
+/// # Example
+///
+/// ```
+/// use jsp::{graph::testdata::build_graph, find};
+/// use std::collections::VecDeque;
+///
+/// let graph = build_graph();
+/// let  search =  VecDeque::from(vec!["show".to_string(), "sequence".to_string(), "shot".to_string()]);
+/// let result = find(search, &graph);
+/// assert!(result.is_ok());
+/// ```
 pub fn find<'a>(criteria: VecDeque<String>, graph: &'a JGraph) -> Result<NodePath<'a>, JSPError> {
     let idx = graph.node_references().next().unwrap().0;
     let  np = vec![idx];
@@ -37,6 +44,7 @@ pub fn find<'a>(criteria: VecDeque<String>, graph: &'a JGraph) -> Result<NodePat
     }
 }
 
+// Internal recursive function
 fn find_recurse<'a>(
     criteria: Rc<RefCell<VecDeque<String>>> ,
     nodepath: Rc<RefCell<Vec<NIndex>>>,
@@ -45,65 +53,74 @@ fn find_recurse<'a>(
     log::info!("");
     log::info!("find_recurse({:?}, {:?})", criteria, nodepath.borrow());
 
+    // explicit lifetime bounds for criteria_borrow. Should test to see
+    // if NLL works here. ie is this needed?
     {
         let criteria_borrow = criteria.borrow();
         if criteria_borrow.len() == 0 {
             return FindValue::Success(nodepath);
         }
     }
-    let component;
+
+    let front_criteria_elem;
     {
         let mut criteria_borrow = criteria.borrow_mut();
-        component = criteria_borrow.pop_front();
-        log::info!("find_recurse component {:?}", component);
+        front_criteria_elem = criteria_borrow.pop_front();
+        log::info!("find_recurse(...) front_criteria_elem {:?}", front_criteria_elem);
     }
-    match component {
-        Some(val) => {
+
+    match front_criteria_elem {
+        Some(candidate_node_name) => {
             let last_node;
             {
                 let np = nodepath.borrow();
                 last_node = np[np.len()-1];
             }
-
-            log::debug!("last node: {:?}", last_node);
+            log::debug!("find_recurse(...) last node: {:?}. Iterating through last_node's children...", last_node);
             for nindex in graph.neighbors(last_node) {
-            //for nindex in neighbors {
-
                 let node = &graph[nindex];
-
-                log::debug!("for nindex in neighbors()... node: {:?}, nindex: {:?}", node, nindex);
-                //cnt += 1;
+                log::debug!("find_recurse(...) for nindex in neighbors()... node: {:?}, nindex: {:?}", node, nindex);
                 match node.identity() {
                     NodeType::RegEx{name, pattern:_, exclude: _} =>  {
-                        log::debug!("match node.indenty() RegEx");
-                        if name == &val {
-                            log::debug!("{} == {}", name, &val);
+                        log::debug!("find_recurse(...) NodeType::RegEx");
+                        if name == &candidate_node_name {
+                            log::debug!("find_recurse(...) {} == {}", name, &candidate_node_name);
                              {
                                  nodepath.borrow_mut().push(nindex);
                              }
                             let r = find_recurse(criteria.clone(), nodepath.clone(), graph);
                             if r.is_success() {
+                                log::debug!("find_recurse(...) successful");
                                 return FindValue::Success(nodepath);
                             } else {
                                 nodepath.borrow_mut().pop();
                             }
                         } else {
-                            log::debug!("{} != {}", name, &val);
+                            log::debug!("find_recurse(...) {} != {}", name, &candidate_node_name);
                         }
                     },
                     NodeType::Simple(_) =>  {
                         log::debug!("NodeType::Simple");
+                        // As this is a simple node, we will do two things Automatically:
+                        // 1) Add the current nindex to the nodepath vector
+                        // 2) Add the candidate_node_name to the front of the candidate VecQueue
+                        // We do this because we accept the Simple nodetype without a means
+                        // test, unlike NodeType::RegEx.
                         {
                             nodepath.borrow_mut().push(nindex);
-                            // we were not able to find
-                            criteria.borrow_mut().push_front(val.clone());
+                            criteria.borrow_mut().push_front(candidate_node_name.clone());
                         }
+
                         let r = find_recurse(criteria.clone(), nodepath.clone(), graph);
                         if r.is_success() {
                             log::debug!("find_recurse successful");
                             return FindValue::Success(nodepath);
                         } else {
                             log::debug!("find_recurse unsuccessful");
+                            // If we did not find what we were looking for
+                            // we pop off the last nodepath item, and we pop off the front
+                            // of the criteria vector, both of which we added prior to calling
+                            // the recursion
                             {
                                 nodepath.borrow_mut().pop();
                                 criteria.borrow_mut().pop_front();
@@ -111,10 +128,12 @@ fn find_recurse<'a>(
                         }
                     },
                     NodeType::Root =>  {
-                        //assert_eq!(nodepath.borrow().len(), 0);
+                        // This should only be added to an empty nodepath
+                        assert_eq!(nodepath.borrow().len(), 0);
                         return find_recurse(criteria.clone(), nodepath.clone(), graph);
                     },
-                    NodeType::Untracked => panic!("should never hit an untracked node in find"),
+                    // If we have gone into untracked territory, then we know we have failed
+                    NodeType::Untracked => {return FindValue::Failure(nodepath);},
                 }
             }
             // made it through all of the children without returning
@@ -123,6 +142,25 @@ fn find_recurse<'a>(
         }
         None => {
             return FindValue::Failure(nodepath);
+        }
+    }
+}
+
+// FindValue is the return type for find_recurse
+// I suppose that i could have used a Result, but It was not clear
+// at the outset what would be needed.
+#[derive(Debug, PartialEq, Eq)]
+enum FindValue {
+    Success(Rc<RefCell<Vec<NIndex>>>),
+    Failure(Rc<RefCell<Vec<NIndex>>>)
+}
+
+impl FindValue {
+    /// Return whether or not the instance of FindValue is Success
+    fn is_success(&self) -> bool {
+        match self {
+            FindValue::Success(_) => true,
+            _ => false,
         }
     }
 }
@@ -139,7 +177,7 @@ mod tests {
     }
 
     #[test]
-    fn can_find_single() {
+    fn will_find_single_criterion() {
         env::set_var("RUST_LOG", "error");
         //env_logger::init();
         init();
@@ -150,7 +188,7 @@ mod tests {
     }
 
     #[test]
-    fn can_find_multi() {
+    fn will_find_multiple_criteria() {
         env::set_var("RUST_LOG", "error");
         //env_logger::init();
         init();
@@ -161,7 +199,7 @@ mod tests {
     }
 
     #[test]
-    fn will_not_find_bs() {
+    fn will_not_succeed_when_given_unmatchable_criteria() {
         env::set_var("RUST_LOG", "error");
         //env_logger::init();
         init();
