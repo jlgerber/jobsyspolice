7fa411027f049b199746fa9c4b01c354fb707d42
Jonathan Gerber <jgerber@jg-mbp-2019.home>
2019-06-12 22:01:17 -0700

    adds JSP_TRACKING_VAR to constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,4 @@
 pub const DEFAULT_USER: &'static str = "jobsys";
 pub const DEFAULT_GROUP: &'static str = "cgi";
 pub const USER_ENV_VAR: &'static str = "USER";
+pub const JSP_TRACKING_VAR: &'static str = "JSP_VARS";
