8b81aa16785d7f7ba6556b31aa9fadf32867fa4b
Jonathan Gerber <jgerber@jg-mbp-2019.home>
2019-06-24 22:33:08 -0700

    cleans up

--- src/bin/jspmk.rs
+++ src/bin/jspmk.rs
@@ -1,14 +1,11 @@
 use chrono;
-use colored::Colorize;
 use dotenv::dotenv;
 use fern::{ colors::{Color, ColoredLevelConfig}, self} ;
-use jsp::*;
+use jsp::{get_graph, mk};
 use log::{ LevelFilter, self };
-use serde_json;
-use std::{ env, ffi::OsString, path::{ Path, PathBuf }, fs::File };
+use std::path::PathBuf;
 use structopt::StructOpt;
 
-const JSP_PATH: &'static str = "JSP_PATH";
 
 #[derive(Debug, StructOpt)]
 #[structopt( name = "jspmk", about = "
@@ -41,17 +38,17 @@ struct Opt {
 }
 
 
-fn main() -> Result<(), JSPError> {
+fn main() -> Result<(), failure::Error> {
     dotenv().ok();
+
     let (args, level) = setup_cli();
+
     setup_logger(level).unwrap();
 
-    let graph = get_graph(false, args.graph);
-    //pub fn mk(mut terms: Vec<String>, graph: &JGraph, full_path: bool, verbose: bool) -> Result<(), JSPError> {
+    let (graph,  _keymap,  _regexmap) = get_graph( args.graph)?;
 
     mk(args.terms, &graph, args.full_path, args.verbose)?;
 
-   
     Ok(())
 }
 
@@ -93,89 +90,4 @@ fn setup_cli() -> (Opt, log::LevelFilter) {
     };
 
     (args, level)
-}
-
-
-#[inline]
-fn _get_template_from_env() -> Result<PathBuf, env::VarError> {
-    let jsp_path = env::var(JSP_PATH)?;
-    log::trace!("expanding tilde for {:?}", jsp_path);
-    let jsp_path = shellexpand::tilde(jsp_path.as_str());
-    log::trace!("attempting to cannonicalize {:?}", jsp_path);
-    let jsp_path = match PathBuf::from(jsp_path.into_owned().as_str()).canonicalize() {
-        Ok(p) => p,
-        Err(e) => {
-            log::error!("failed to cannonicalize {}", e);
-            // Todo swap this out when implement failure
-            return Err(env::VarError::NotPresent);
-        }
-    };
-    log::trace!("returning {:?}", jsp_path);
-    Ok(jsp_path)
-}
-
-#[inline]
-fn get_template_from_env() -> PathBuf {
-    match _get_template_from_env() {
-        Ok(v) => v,
-        Err(e) => {
-            eprintln!("\nunable to get template from environment: {}. Is {} set?\n", e, JSP_PATH);
-            std::process::exit(1);
-        }
-    }
-}
-
-#[inline]
-fn open_template(template: &Path) -> File {
-    match File::open(&template) {
-        Ok(f) => f,
-        Err(e) => {
-            eprintln!("\nunable to open {:?}. error: {}\n", template, e);
-            std::process::exit(1);
-        }
-    }
-}
-
-#[inline]
-fn _get_graph(graph: Option<PathBuf>) -> JGraph {
-    if graph.is_none() {
-        let template = get_template_from_env();
-        let json_file = open_template(&template);
-        let result: JGraph =
-        serde_json::from_reader(json_file).expect("error while reading json");
-        result
-    } else {
-        let json_file_path = graph.unwrap();
-        let json_file = File::open(json_file_path).expect("file not found");
-        let result: JGraph =
-        serde_json::from_reader(json_file).expect("error while reading json");
-        result
-    }
-}
-
-#[inline]
-fn get_graph(has_output: bool, graph: Option<PathBuf>) -> JGraph {
-    if has_output {
-        graph::testdata::build_graph()
-    } else {
-         _get_graph(graph)
-    }
-}
-
-
-#[inline]
-fn report_failure(input: &Path, entry: &OsString, node: NIndex, depth: u8, graph: &JGraph, verbose: bool) {
-    let path = input
-                .iter()
-                .take((depth+1) as usize)
-                .fold(PathBuf::new(), |mut p, v| {p.push(v); p});
-
-    let neighbors = graph.neighbors(node);
-    if verbose { eprintln!("\n{}\n", "Failure".bright_red()); }
-    eprintln!("Failed to match {} in {:?} against:", entry.to_str().unwrap().bright_blue(), path);
-    for n in neighbors {
-        eprintln!("\t{}", graph[n].display_name().bright_blue());
-    }
-    if verbose { eprintln!(""); }
-    std::process::exit(1);
 }
