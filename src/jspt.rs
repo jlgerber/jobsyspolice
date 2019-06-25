bde1e299fca823a0f1f5d4e241b0920bb0c22305
Jonathan Gerber <jgerber@jg-mbp-2019.home>
2019-06-24 22:32:29 -0700

    adds note to jspt

--- src/jspt.rs
+++ src/jspt.rs
@@ -1,4 +1,8 @@
-
+//! Formerly a separate crate - jspcompile - this code was merged when
+//! it was discovered that there was no nead to precompile the file into
+//! a json struct. The parsing was fast enough. 
+//! 
+//! The code remains distinct however.
 pub mod parser;
 pub use parser::{start_parser, regex_parser, node_parser, edge_parser, parse_components, parse_metadata};
 
