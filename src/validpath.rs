39ce95a1a5152e1ab7914cc9f49e2e23e6466ebe
Jonathan Gerber <jgerber@jg-mbp-2019.home>
2019-06-29 17:26:46 -0700

    adds optional conversion to absolute path

--- src/validpath.rs
+++ src/validpath.rs
@@ -1,8 +1,9 @@
 use std::{fmt, path::{Path, PathBuf}};
-use crate::{NodePath, JGraph, JSPError, validate_path};
+use crate::{diskutils, NodePath, JGraph, JSPError, validate_path, find_path, find_path_from_terms, SearchTerm, Search};
 use std::fmt::Debug;
 
-/// A ValidPath provides a path that has been validated againt the template. 
+/// A ValidPath provides a pairing of a PathBuf and a NodePath, representing a path that 
+/// has been validated against the JGraph. 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct ValidPath<'a> {
     pathbuf: PathBuf,
@@ -18,10 +19,20 @@ impl<'a> fmt::Display for ValidPath<'a> {
 
 impl<'a> ValidPath<'a> {
 
-    /// Construct a ValidPath from a pathbuf
-    pub fn new<I: Into<PathBuf>>(pathbuf: I, graph: &'a JGraph) -> Result<ValidPath<'a>, JSPError> {
+    /// New up a ValidPath from a PathBuf. 
+    /// 
+    /// # Parameters
+    /// 
+    /// * `pathbuf` - A `PathBuf` instance
+    /// * `graph` - Reference to the JGraph instance 
+    /// * `absolute` - Whether to convert the ValidPath into an absolute path or not. 
+    /// 
+    /// # Returns
+    /// Ok wrapping ValidPath instance if Successful. 
+    /// Otherwise, an Err wrapping JSPError
+    pub fn new<I: Into<PathBuf>>(pathbuf: I, graph: &'a JGraph, absolute: bool) -> Result<ValidPath<'a>, JSPError> {
 
-        let pathbuf = pathbuf.into();
+        let pathbuf = if absolute{diskutils::convert_relative_pathbuf_to_absolute(pathbuf.into())?} else {pathbuf.into()};
         let nodepath = validate_path(&pathbuf, graph)?;
 
         Ok(ValidPath {
@@ -30,15 +41,85 @@ impl<'a> ValidPath<'a> {
         })
     }
 
-    /// Take responsibility for validation. Take one PathBuf, add one NodePath, and voila
-    pub fn new_unchecked<I: Into<PathBuf>>(pathbuf: I, nodepath: NodePath<'a>) -> ValidPath<'a> {
-        let pathbuf = pathbuf.into();
-       
-        ValidPath {
+    /// New up a ValidPath from components. This method is dangerous and should only be used if one knows that the 
+    /// validation has taken place.  
+    /// Take one PathBuf, add one NodePath, and voila
+    /// 
+    /// # Parameters
+    /// 
+    /// * `pathbuf` - a type that is `Into<PathBuf>`
+    /// * `nodepath` - A NodePath instance
+    /// * `absolute` - Whether to convert the ValidPath into an absolute path or not. 
+    /// 
+    /// # Returns
+    /// Ok wrapping ValidPath instance if Successful. 
+    /// Otherwise, an Err wrapping JSPError
+    pub fn new_unchecked<I: Into<PathBuf>>(pathbuf: I, nodepath: NodePath<'a>,  absolute: bool) -> Result<ValidPath<'a>, JSPError> {
+        let pathbuf = if absolute{diskutils::convert_relative_pathbuf_to_absolute(pathbuf.into())?} else {pathbuf.into()};
+       Ok(
+            ValidPath {
+                pathbuf, 
+                nodepath
+            }
+       )
+    }
+
+    /// New up a ValidPath from a vector of SearchTerm
+    /// 
+    /// # Parameters
+    /// 
+    /// * `terms` - A vector of `SearchTearm`s, representing an ordered (though sparse) traversal of the JGraph
+    /// * `graph` - Reference to the JGraph instance 
+    /// * `absolute` - Whether to convert the ValidPath into an absolute path or not. 
+    /// 
+    /// # Returns
+    /// Ok wrapping ValidPath instance if Successful. 
+    /// Otherwise, an Err wrapping JSPError
+    pub fn new_from_searchterms(terms: Vec<SearchTerm>, graph: &'a JGraph, absolute: bool) -> Result<ValidPath<'a>, JSPError> {
+        let (pathbuf, nodepath) = find_path_from_terms(terms, graph)?;
+        let pathbuf = if absolute{diskutils::convert_relative_pathbuf_to_absolute(pathbuf)?} else {pathbuf};
+
+        Ok(ValidPath {
             pathbuf, 
             nodepath
-        }
+        })
     }
+    
+    /// New up a ValidPath from a Search reference
+    /// 
+    /// # Parameters
+    /// 
+    /// * `search` - Reference to `Search`, containing a list of `SearchTerm`s 
+    /// * `graph` - Reference to the JGraph instance 
+    /// * `absolute` - Whether to convert the ValidPath into an absolute path or not. 
+    ///  
+    /// # Returns
+    /// Ok wrapping ValidPath instance if Successful. 
+    /// Otherwise, an Err wrapping JSPError
+    pub fn new_from_search(search: &Search, graph: &'a JGraph, absolute: bool) -> Result<ValidPath<'a>, JSPError> {
+        let (pathbuf, nodepath) = find_path(search, graph)?;
+        let pathbuf = if absolute{diskutils::convert_relative_pathbuf_to_absolute(pathbuf)?} else {pathbuf};
+
+        Ok(ValidPath {
+            pathbuf, 
+            nodepath
+        })
+    }
+
+    // pub fn absolute(mut self) -> Result<Self, JSPError> {
+    //     if let mut ValidPath{pathbuf, nodepath} = self {
+
+    //         let pathbuf = diskutils::convert_relative_pathbuf_to_absolute(pathbuf)?;
+    //         Ok(
+    //             PathBuf{
+    //                 pathbuf,
+    //                 nodepath
+    //             }
+    //         )
+    //     } else {
+    //         Err(JSPError::Placeholder)
+    //     }
+    // }
 
     /// Return a reference to a Path
     pub fn path(&self) -> &Path {
