33d4bbc1963cac8856ab47cf4d53ed43e9a70bda
Jonathan Gerber <jgerber@jg-mbp-2019.home>
2019-06-30 12:00:52 -0700

    adds jsperror. removes report out of names of functions

--- src/report.rs
+++ src/report.rs
@@ -1,7 +1,8 @@
-use crate::{JGraph, NIndex, NodePath, ValidPath};
+use crate::{JGraph, NIndex, NodePath, ValidPath, JSPError};
+use colored::*;
+use log;
 use std::path::{PathBuf, Path};
 use std::ffi::{OsString, OsStr};
-use colored::*;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Success<'a> {
@@ -12,6 +13,7 @@ pub enum Success<'a> {
 pub fn mk_success(path: &Path, verbose:bool ) {
     let cr = if verbose {"\n"} else {""};
     println!("{}{} {}{}", cr, "Created:".bright_blue(), path.display(), cr);
+    log::info!("mk_success. Created {}", path.display());
 }
 
 /// Report successful execution of task to user. 
@@ -24,33 +26,35 @@ pub fn mk_success(path: &Path, verbose:bool ) {
 /// None
 pub fn validate_success(nodepath: NodePath) {
     eprintln!("\nSuccess\n");
-
     for n in nodepath.iter() {
         eprintln!("{:?}", n.display_name());
     }
-
+    
     println!();
+    log::info!("validate_success. Success: {:?}", nodepath);
 }
 
 /// Report failed task to user, given context
-pub fn report_failure(input: &OsStr, entry: &OsString, node: NIndex, depth: u8, graph: &JGraph, verbose: bool ) {
+pub fn failure(input: &OsStr, entry: &OsString, node: NIndex, depth: u8, graph: &JGraph, verbose: bool ) {
     let path = Path::new(input)
                 .iter()
                 .take((depth+1) as usize)
                 .fold(PathBuf::new(), |mut p, v| {p.push(v); p});
 
     let neighbors = graph.neighbors(node);
+    let entry_str = entry.to_str().unwrap_or("");
     if verbose { eprintln!("\n{}\n", "Failure".bright_red()); }
-    eprintln!("Failed to match {} in {:?} against:", entry.to_str().unwrap_or("").bright_red(), path);
+    eprintln!("Failed to match {} in {:?} against:", entry_str.bright_red(), &path);
     for n in neighbors {
         eprintln!("{}", graph[n].display_name().bright_red());
     }
     if verbose { eprintln!(""); }
+    log::error!("report_failure. Failed to match {} in {:?}", entry_str, &path);
     std::process::exit(1);
 }
 
 /// Report simple failure to the user given an error str and a verbose bool
-pub fn report_simple_failure(error: &str, verbose: bool ) {
+pub fn simple_failure(error: &str, verbose: bool ) {
     if verbose { 
         eprintln!("\n{}\n", "Error".bright_red()); 
         eprintln!("\t{}", error);
@@ -58,5 +62,38 @@ pub fn report_simple_failure(error: &str, verbose: bool ) {
     } else {
         eprintln!("{} {}", "Error".bright_red(), error);
     }
+    log::error!("report_simple_failure. {}", error);
+    // what am i doing here? this should't be in log
     std::process::exit(1);
+}
+
+/// Report a jsperror
+/// 
+/// # Parameters
+/// * `info` - contextual information str from caller
+/// * `error` - underlying JSPError
+/// * `verbose` - whether to make the error reporting verbose or not
+/// 
+/// # Returns
+/// None
+pub fn jsperror(info: &str, error: JSPError, verbose: bool) {
+    let error_str = error.to_string();
+    if verbose { 
+        eprintln!("\n{}\n", "Error".bright_red()); 
+        eprintln!("\t{} '{}'", info, error_str);
+        eprintln!("");
+    } else {
+        eprintln!("{} {} '{}'", "Error".bright_red(), info, error_str);
+    }
+    log::error!("jsperror. {} {}", info, error_str);
+}
+
+pub(crate) fn go_failure(path_str: &str, myshell: bool, verbose: bool) {
+    let cr = if verbose { "\n" } else {""};
+    if !myshell {
+        eprintln!("echo {}Error: Path does not exist: {}{}", cr, path_str.bright_blue(), cr);
+    } else {
+        eprintln!("{}Error: Path does not exist: '{}'{}", cr, path_str.bright_blue(), cr);
+    }
+    log::error!("go_failure: Path does not exist: '{}'", path_str);
 }
