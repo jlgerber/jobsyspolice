ab3a9e535ef4e1d1c220ca6d5e641ac6e55b1c3d
Jonathan Gerber <jgerber@jg-mbp-2019.home>
2019-07-03 22:50:00 -0700

    fix bash script to handle errors

--- setup.bash
+++ setup.bash
@@ -1,3 +1,12 @@
 cmd="jsp go --shell=bash"
-function jspgo { eval `$cmd "$@"` ; }
+
+function jspgo { 
+    FOO=$($cmd "$@") > /dev/null; 
+    if [[ $FOO =~ \s*Error.* ]] ;
+    then
+        echo $FOO 
+    else
+        eval $FOO
+    fi
+}
 export JSP_PATH=~/etc/template.jspt
