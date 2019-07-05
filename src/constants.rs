ccd7b371a0bbb17c09c0c7efb0f18eb22f7c82ec
Jonathan Gerber <jgerber@jg-mbp-2019.home>
2019-07-05 13:31:32 -0700

    adds const envvars for show, seq,shot, root

--- src/constants.rs
+++ src/constants.rs
@@ -1,7 +1,12 @@
-pub const DEFAULT_USER: &str = "jobsys";
-pub const DEFAULT_GROUP: &str = "cgi";
-pub const USER_ENV_VAR: &str = "USER";
+pub const DEFAULT_USER:     &str = "jobsys";
+pub const DEFAULT_GROUP:    &str = "cgi";
+pub const USER_ENV_VAR:     &str = "USER";
 pub const JSP_TRACKING_VAR: &str = "JSP_VARS";
-pub const JSP_PATH: &str = "JSP_PATH";
-pub const JSP_NAME: &str = "template.jspt";
-pub const LEVELS: &[&str] = &["show", "sequence", "shot"];
+pub const JSP_PATH:         &str = "JSP_PATH";
+pub const JSP_NAME:         &str = "template.jspt";
+pub const LEVELS:        &[&str] = &["show", "sequence", "shot"];
+
+pub const JSP_SHOWS_ROOT_ENVVAR: &str = "DD_SHOWS_ROOT";
+pub const JSP_SHOW_ENVVAR:       &str = "DD_SHOW";
+pub const JSP_SEQUENCE_ENVVAR:   &str = "DD_SEQUENCE";
+pub const JSP_SHOT_ENVVAR:       &str = "DD_SHOT";
