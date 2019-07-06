db5292a64f0635586db58b7fe248ec28f6a1e444
jgerber <jlgerber@gmail.com>
2019-07-05 21:16:16 -0700

    adds Navalias::new_simple and Navalias::new_complex

--- src/navalias.rs
+++ src/navalias.rs
@@ -5,3 +5,21 @@ pub enum Navalias {
     Simple(String),
     Complex{name:String, value:String},
 }
+
+impl Navalias {
+    /// Instantiate a new Navalias::Simple
+    pub fn new_simple<S>(name: S) -> Self 
+    where
+        S: Into<String>
+    {
+        Navalias::Simple(name.into())
+    } 
+
+    /// Instantiate a new Navalias::Complex 
+    pub fn new_complex<S>(name: S, value: S) -> Self 
+    where
+        S: Into<String> 
+    {
+        Navalias::Complex{name: name.into(), value: value.into()}
+    }
+}
