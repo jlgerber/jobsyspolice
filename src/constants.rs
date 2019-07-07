0c2799930720e370dac8618593c854a864133fab
jgerber <jlgerber@gmail.com>
2019-07-07 07:06:35 -0700

    adds JSP_ALIAS_NAMES which track aliases

--- src/constants.rs
+++ src/constants.rs
@@ -2,6 +2,8 @@ pub const DEFAULT_USER:     &str = "jobsys";
 pub const DEFAULT_GROUP:    &str = "cgi";
 pub const USER_ENV_VAR:     &str = "USER";
 pub const JSP_TRACKING_VAR: &str = "JSP_VARS";
+pub const JSP_ALIAS_NAMES:  &str = "JSP_ALIAS_NAMES";
+
 pub const JSP_PATH:         &str = "JSP_PATH";
 pub const JSP_NAME:         &str = "template.jspt";
 pub const LEVELS:        &[&str] = &["show", "sequence", "shot"];
